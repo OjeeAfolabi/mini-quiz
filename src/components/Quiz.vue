c4c31c72956c972caa538aab65acbc02d5f25d79
Ojee Afolabi <sylvesterojay@gmail.com>
2025-02-17 21:26:02 +0100

    to correct android functions

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -18,11 +18,34 @@ const onDrop = (event, questions) => {
   } else {
     alert('Wrong!')
   }
-}
+};
 
 const onDragStart = (event, answer) => {
   event.dataTransfer.setData('answerId', answer.id)
-}
+};
+
+const onTouchStart = (event, answer) => {
+  event.dataTransfer = {
+    setData: (type, id) => {
+      event.dataTransfer[type] = id;
+    },
+    getData: (type) => {
+      return event.dataTransfer[type];
+    },
+  };
+  event.dataTransfer.setData('answerId', answer.id);
+};
+
+const onTouchEnd = (event, question) => {
+  const answerId = event.dataTransfer.getData('answerId');
+  const answer = answers.value.find(a => a.id == answerId);
+  if (answer && answer.text === question.answer) {
+    alert('Correct!');
+  } else {
+    alert('Wrong!');
+  }
+};
+
 </script>
 
 <template>
@@ -33,6 +56,7 @@ const onDragStart = (event, answer) => {
         class="question md:text-xl sm:text-base md:mt-1 sm:mt-0.5"
         @drop.prevent="(event) => onDrop(event, questions)"
         @dragover.prevent
+        @touchend.prevent="(event) => onTouchEnd(event, questions)"
       >
         {{ questions.question }}
       </div>
@@ -45,6 +69,7 @@ const onDragStart = (event, answer) => {
         class="answer border-1 border-amber-500 md:p-2 sm:p-1 rounded md:mt-6 sm:mt-2.5 hover:bg-amber-200"
         draggable="true"
         @dragstart="(event) => onDragStart(event, answer)"
+        @touchstart="(event) => onTouchStart(event, answer)"
       > {{ answer.option}}
         {{ answer.text}}
       </div>
